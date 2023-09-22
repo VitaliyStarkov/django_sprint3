f4c8decc84d38bf0e8f9ed546d1ceb9941d9c65f
Старков Виталий <vetalsta@gmail.com>
2023-09-22 04:43:00 +0300

    v1

--- blogicum/blog/models.py
+++ blogicum/blog/models.py
@@ -39,10 +39,10 @@ class Post(PublishedModel):
     title = models.CharField(max_length=256,
                              verbose_name='Заголовок')
     text = models.TextField(verbose_name='Текст')
-    pub_date = models.DateTimeField(
-                    verbose_name='Дата и время публикации',
-                    help_text=('Если установить дату и время в будущем —'
-                               'можно делать отложенные публикации.'))
+    pub_date = models.DateTimeField(verbose_name='Дата и время публикации',
+                                    help_text=('Если установить дату и время'
+                                               'в будущем — можно делать'
+                                               'отложенные публикации.'))
     author = models.ForeignKey(
         User,
         on_delete=models.CASCADE,
