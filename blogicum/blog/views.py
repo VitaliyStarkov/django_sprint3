47e74fc413fe36f94f2affb7fa456118bd48c57b
Старков Виталий <vetalsta@gmail.com>
2023-09-25 04:30:05 +0300

    2nd review

--- blogicum/blog/views.py
+++ blogicum/blog/views.py
@@ -7,39 +7,39 @@ from .models import Post, Category
 
 def filter_and_order_posts(post_manager):
     return post_manager.filter(
-                            is_published=True,
-                            category__is_published=True,
-                            pub_date__lt=timezone.now()
-                             ).select_related('author', 'location',
-                                              'category')
+        is_published=True,
+        category__is_published=True,
+        pub_date__lt=timezone.now()
+    ).select_related('author', 'location',
+                     'category')
 
 
 def index(request):
     posts = filter_and_order_posts(Post.objects)
     return render(
-               request,
-               'blog/index.html',
-               {'post_list': posts[:MAX_AMOUNT_POSTS]}
-                )
+        request,
+        'blog/index.html',
+        {'post_list': posts[:MAX_AMOUNT_POSTS]}
+    )
 
 
 def post_detail(request, post_id):
     post = get_object_or_404(
-                          filter_and_order_posts(Post.objects),
-                          pk=post_id
-                           )
+        filter_and_order_posts(Post.objects),
+        pk=post_id
+    )
     return render(request, 'blog/detail.html', {'post': post})
 
 
 def category_posts(request, category_slug):
     category = get_object_or_404(
-                              Category,
-                              slug=category_slug,
-                              is_published=True
-                               )
+        Category,
+        slug=category_slug,
+        is_published=True
+    )
     post_list = filter_and_order_posts(category.posts)
     return render(
-               request,
-               'blog/category.html',
-               {'category': category, 'post_list': post_list}
-                )
+        request,
+        'blog/category.html',
+        {'category': category, 'post_list': post_list}
+    )
