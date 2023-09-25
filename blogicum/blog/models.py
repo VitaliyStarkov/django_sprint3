7f13848ca3ee04ac93da7150d83f88bbdae1f511
Старков Виталий <vetalsta@gmail.com>
2023-09-25 04:18:41 +0300

    2nd review

--- blogicum/blog/models.py
+++ blogicum/blog/models.py
@@ -9,16 +9,16 @@ User = get_user_model()
 
 class Category(PublishedCreated):
     title = models.CharField(
-                           'Заголовок',
-                           max_length=MAX_LENGTH_TITLE
+                          'Заголовок',
+                          max_length=MAX_LENGTH_TITLE
                            )
     description = models.TextField('Описание')
     slug = models.SlugField(
-                          'Идентификатор',
-                          unique=True,
-                          help_text=('Идентификатор страницы для URL; '
-                                     'разрешены символы латиницы, цифры, '
-                                     'дефис и подчёркивание.')
+                         'Идентификатор',
+                         unique=True,
+                         help_text=('Идентификатор страницы для URL; '
+                                    'разрешены символы латиницы, цифры, '
+                                    'дефис и подчёркивание.')
                           )
 
     class Meta:
@@ -31,8 +31,8 @@ class Category(PublishedCreated):
 
 class Location(PublishedCreated):
     name = models.CharField(
-                          'Название места',
-                          max_length=MAX_LENGTH_TITLE
+                         'Название места',
+                         max_length=MAX_LENGTH_TITLE
                           )
 
     class Meta:
@@ -45,23 +45,23 @@ class Location(PublishedCreated):
 
 class Post(PublishedCreated):
     title = models.CharField(
-                           'Заголовок',
-                           max_length=MAX_LENGTH_TITLE
+                          'Заголовок',
+                          max_length=MAX_LENGTH_TITLE
                            )
     text = models.TextField('Текст')
     pub_date = models.DateTimeField(
-                                  'Дата и время публикации',
-                                  help_text=('Если установить '
-                                             'дату и время в будущем '
-                                             '— можно делать отложенные '
-                                             'публикации.')
+                                 'Дата и время публикации',
+                                 help_text=('Если установить '
+                                            'дату и время в будущем '
+                                            '— можно делать отложенные '
+                                            'публикации.')
                                   )
     author = models.ForeignKey(
                             User,
                             on_delete=models.CASCADE,
                             related_name='posts',
                             verbose_name='Автор публикации'
-                            )
+                             )
     location = models.ForeignKey(
                               Location,
                               on_delete=models.SET_NULL,
@@ -69,14 +69,14 @@ class Post(PublishedCreated):
                               null=True,
                               related_name='posts',
                               verbose_name='Местоположение'
-                              )
+                               )
     category = models.ForeignKey(
                               Category,
                               on_delete=models.SET_NULL,
                               null=True,
                               related_name='posts',
                               verbose_name='Категория'
-                              )
+                               )
 
     class Meta:
         verbose_name = 'публикация'
